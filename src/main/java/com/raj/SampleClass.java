0447bd90f97818f14b6f0ff7e695f392fa1939b8
rajkiranreddym <kiran.munnangi@gmail.com>
2017-02-11 19:14:57 -0600

    First commit

---
+++ src/main/java/com/raj/SampleClass.java
@@ -0,0 +1,7 @@
+package com.raj;
+
+public class SampleClass {
+public String sampleMethod(){
+	return "sample method";
+}
+}
---
+++ src/test/java/com/raj/SampleClassTest.java
@@ -0,0 +1,14 @@
+package com.raj;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SampleClassTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
